1ff6785adc8e1cbd6eb970634032e9ddebaa24fe
zhuhaow <zhuhaow@gmail.com>
2021-11-11 14:07:48 +0800

    Init project

---
+++ Specht2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Specht2
+//
+//  Created by Zhuhao Wang on 2021/11/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Specht2/Specht2App.swift
@@ -0,0 +1,17 @@
+//
+//  Specht2App.swift
+//  Specht2
+//
+//  Created by Zhuhao Wang on 2021/11/11.
+//
+
+import SwiftUI
+
+@main
+struct Specht2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
