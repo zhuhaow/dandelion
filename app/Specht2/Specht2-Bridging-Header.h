2bc42b086073ee79deff41c06cdda19a5067c0c9
zhuhaow <zhuhaow@gmail.com>
2021-11-28 00:13:10 +0800

    Set up cbindgen for xcode

---
+++ app/Specht2/Specht2-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Specht2-Bridging-Header.h
+//  Specht2
+//
+//  Created by Zhuhao Wang on 2021/11/28.
+//
+
+#ifndef Specht2_Bridging_Header_h
+#define Specht2_Bridging_Header_h
+
+#include "core.h"
+
+#endif /* Specht2_Bridging_Header_h */
