55392d3b27c8562cce5b7c2ef5fcb39f571109b9
zhuhaow <zhuhaow@gmail.com>
2021-12-16 18:43:40 +0800

    Refactored FFI

--- core/src/binding.rs
+++ core/src/binding.rs
@@ -6,23 +6,19 @@ use std::{
     fs::read_to_string,
     os::raw::c_char,
     path::PathBuf,
-    ptr::null,
+    ptr::{null, NonNull},
     thread,
 };
-use tokio::{runtime::Builder, select, sync::broadcast};
-
-lazy_static::lazy_static! {
-    static ref SIGNAL_CHANNEL: broadcast::Sender<()> = broadcast::channel(1).0;
-}
+use tokio::{runtime::Builder, select, sync::oneshot};
 
 #[repr(C)]
 pub struct CompletedCallback {
-    userdata: *mut c_void,
+    userdata: NonNull<c_void>,
     // The char ptr points to an error string if there is an error. If it's
     // null, then the action completes successfully. The callback should copy
     // the error string immediately if needed since it will be released after
     // the callback.
-    callback: extern "C" fn(*mut c_void, *const c_char),
+    callback: extern "C" fn(NonNull<c_void>, *const c_char),
 }
 
 unsafe impl Send for CompletedCallback {}
@@ -34,7 +30,7 @@ impl CompletedCallback {
         (self.callback)(
             self.userdata,
             match err_str {
-                Some(s) => s.as_ptr(),
+                Some(ref s) => s.as_ptr(),
                 None => null(),
             },
         );
@@ -54,11 +50,19 @@ impl Drop for CompletedCallback {
 ///
 /// The function won't take the ownership of the passed in config path string.
 /// The callback may be called from any thread. Pay attention to synchronization.
+///
+/// The returned stop handler must be released by calling stop with the pointer no
+/// matter the server is already stopped because of an error or not.
 #[no_mangle]
-pub unsafe extern "C" fn start(config_path: *const c_char, callback: CompletedCallback) {
-    let path_string = CStr::from_ptr(config_path).to_string_lossy().into_owned();
+pub unsafe extern "C" fn specht2_start(
+    config_path: NonNull<c_char>,
+    callback: CompletedCallback,
+) -> NonNull<c_void> {
+    let path_string = CStr::from_ptr(config_path.as_ptr())
+        .to_string_lossy()
+        .into_owned();
     let path = PathBuf::from(path_string);
-    let mut subscriber = SIGNAL_CHANNEL.subscribe();
+    let (tx, rx) = oneshot::channel::<()>();
 
     thread::spawn(move || {
         let runtime = Builder::new_multi_thread()
@@ -73,15 +77,19 @@ pub unsafe extern "C" fn start(config_path: *const c_char, callback: CompletedCa
 
             select! {
                 result = server.serve() => result,
-                _ = subscriber.recv() => Ok(()),
+                _ = rx => Ok(()),
             }
         });
 
         callback.done(result.err().map(|e| e.to_string()));
     });
+
+    let tx = Box::into_raw(Box::new(tx));
+    NonNull::new_unchecked(tx as *mut c_void)
 }
 
 #[no_mangle]
-pub extern "C" fn stop() -> bool {
-    SIGNAL_CHANNEL.send(()).is_ok()
+pub extern "C" fn specht2_stop(sender: NonNull<c_void>) -> bool {
+    let sender = unsafe { Box::from_raw(sender.as_ptr() as *mut oneshot::Sender<()>) };
+    sender.send(()).is_ok()
 }
