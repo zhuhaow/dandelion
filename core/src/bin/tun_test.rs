17d59cf37fc9cadf285750e628f1033ab7fc188f
zhuhaow <zhuhaow@gmail.com>
2022-01-11 17:24:07 +0800

    Fix tun_test for new API

--- core/src/bin/tun_test.rs
+++ core/src/bin/tun_test.rs
@@ -3,10 +3,8 @@ use specht2_core::Result;
 #[cfg(target_os = "macos")]
 #[tokio::main]
 async fn main() -> Result<()> {
-    use std::{sync::Arc, time::Duration};
-
     use ipnetwork::Ipv4Network;
-    use specht2_core::tun::{device::Device, dns::FakeDns, stack::run_stack, TranslatorConfig};
+    use specht2_core::tun::{device::Device, stack::create_stack};
 
     flexi_logger::Logger::try_with_env()
         .unwrap()
@@ -16,31 +14,10 @@ async fn main() -> Result<()> {
     let device = Device::new("10.128.0.1/12".parse().unwrap())?;
 
     let ip_block: Ipv4Network = "10.128.0.1/12".parse().unwrap();
-    let mut ip_iter = ip_block.into_iter();
 
-    let fake_ips = (&mut ip_iter)
-        .take(10)
-        .map(|ip| match ip {
-            std::net::IpAddr::V4(ip) => ip,
-            std::net::IpAddr::V6(_) => unreachable!(),
-        })
-        .collect();
-    let fake_ports = 1024..65535;
-    let translator_config = TranslatorConfig::new(
-        "127.0.0.1:8091".parse().unwrap(),
-        fake_ips,
-        fake_ports,
-        Duration::from_secs(180),
-    );
+    let stack = create_stack(device, ip_block, "127.0.0.1:8091".parse().unwrap()).await?;
 
-    run_stack(
-        device,
-        Arc::new(FakeDns::new("8.8.8.8:53".parse().unwrap(), ip_iter).await?),
-        "10.128.0.1:53".parse().unwrap(),
-        translator_config,
-        1500,
-    )
-    .await?;
+    stack.0.await;
 
     Ok(())
 }
