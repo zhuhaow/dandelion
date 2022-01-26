0502dedddae617a4c8e13316b426e9ba86959a0f
zhuhaow <zhuhaow@gmail.com>
2022-01-26 13:10:34 +0800

    Fix doc

--- core/src/connector/rule/domain.rs
+++ core/src/connector/rule/domain.rs
@@ -26,8 +26,8 @@ impl<C: Connector> DomainRule<C> {
 impl<C: Connector> Rule<C> for DomainRule<C> {
     async fn check(&self, endpoint: &Endpoint) -> Option<&C> {
         if let Endpoint::Domain(d, _) = endpoint {
-            // The domain should be FQDN but may come with two forms, w/o the
-            // ending dot. We don't want the user to deal with that.
+            // The domain should be FQDN but may come with two forms, w/ or w/o
+            // the ending dot. We don't want the user to deal with that.
             let d = d.strip_suffix('.').unwrap_or(d);
             for mode in self.modes.iter() {
                 if match mode {
