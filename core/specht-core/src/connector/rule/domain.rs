14c4deef1173bf3c874da9dd79e95f8ee6f2ab7d
zhuhaow <zhuhaow@gmail.com>
2022-02-05 18:57:34 +0800

    Don't do unnecessary domain preprocess when matching domain

--- core/specht-core/src/connector/rule/domain.rs
+++ core/specht-core/src/connector/rule/domain.rs
@@ -28,18 +28,6 @@ impl<C: Connector> Rule<C> for DomainRule<C> {
     async fn check(&self, endpoint: &Endpoint) -> Option<&C> {
         if let Endpoint::Domain(d, _) = endpoint {
             debug!("Matching endpoint with domain {}", d);
-            // The domain should be FQDN but may come with two forms, w/ or w/o
-            // the ending dot. We don't want the user to deal with that.
-            let d = d
-                .strip_suffix('.')
-                .map(|d| {
-                    debug!(
-                        "Removing the ending dot from domain, matching with new domain {}",
-                        d
-                    );
-                    d
-                })
-                .unwrap_or(d);
             for mode in self.modes.iter() {
                 if match mode {
                     Mode::Prefix(p) => d.starts_with(p),
