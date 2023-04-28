4ba296005b0443153c3cf5692eab59cb9bf97492
zhuhaow <zhuhaow@gmail.com>
2023-04-28 16:17:32 +0800

    refactor: Simplify function generic param

--- core/specht-core/src/acceptor/mod.rs
+++ core/specht-core/src/acceptor/mod.rs
@@ -6,30 +6,22 @@ use futures::{Future, Stream, TryFutureExt, TryStreamExt};
 
 pub fn handle_connection_stream<
     Input: Io,
-    Output: Io,
-    InputStream: Stream<Item = Result<Input>>,
-    F1: Future<Output = Result<(Endpoint, F2)>>,
-    F2: Future<Output = Result<Output>>,
-    Handshake: Fn(Input) -> F1 + 'static,
+    F: Future<Output = Result<(Endpoint, impl Future<Output = Result<impl Io>>)>>,
 >(
-    s: InputStream,
-    handshake: Handshake,
-) -> impl Stream<Item = Result<Result<(Endpoint, F2)>>> {
+    s: impl Stream<Item = Result<Input>>,
+    handshake: impl Fn(Input) -> F + 'static,
+) -> impl Stream<Item = Result<Result<(Endpoint, impl Future<Output = Result<impl Io>>)>>> {
     s.and_then(move |io| handshake(io).map_ok(Ok))
 }
 
 pub fn handle_connection_stream_with_config<
     Input: Io,
-    Output: Io,
     C,
-    InputStream: Stream<Item = Result<Input>>,
-    F1: Future<Output = Result<(Endpoint, F2)>>,
-    F2: Future<Output = Result<Output>>,
-    Handshake: Fn(Input, &C) -> F1,
+    F: Future<Output = Result<(Endpoint, impl Future<Output = Result<impl Io>>)>>,
 >(
-    s: InputStream,
-    handshake: Handshake,
+    s: impl Stream<Item = Result<Input>>,
+    handshake: impl Fn(Input, &C) -> F,
     config: C,
-) -> impl Stream<Item = Result<Result<(Endpoint, F2)>>> {
+) -> impl Stream<Item = Result<Result<(Endpoint, impl Future<Output = Result<impl Io>>)>>> {
     s.and_then(move |io| handshake(io, &config).map_ok(Ok))
 }
