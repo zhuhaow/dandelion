f6fc40e08da9be9e02e1731eee77e3a887010b71
zhuhaow <zhuhaow@gmail.com>
2024-05-05 21:27:53 +0800

    Make quic only a transimission protocol

--- core/dandelion-core/src/connector/quic.rs
+++ core/dandelion-core/src/connector/quic.rs
@@ -14,30 +14,15 @@ pub struct QuicConnection {
 
 pub async fn create_quic_connection<R: Resolver>(
     server: Endpoint,
-    token: &str,
     resolver: R,
 ) -> Result<QuicConnection> {
     Ok(QuicConnection {
-        inner: client_connect(server, token, resolver).await?,
+        inner: client_connect(server, resolver).await?,
     })
 }
 
 pub async fn connect(endpoint: &Endpoint, connection: &QuicConnection) -> Result<QuicStream> {
     let (mut send, mut recv) = connection.inner.open_bi().await?;
-
-    let endpoint_str = endpoint.to_string();
-    let len = endpoint_str.as_bytes().len();
-    ensure!(len < u8::MAX as usize, "endpoint {} is too long", endpoint);
-
-    send.write_u8(len as u8).await?;
-    send.write_all(endpoint_str.as_bytes()).await?;
-
-    if recv.read_u8().await? != QuicMessage::Ok as u8 {
-        bail!(
-            "Failed to connect to endpoint {} from remote server",
-            endpoint
-        )
-    }
 
     Ok(QuicStream::new(send, recv))
 }
