a9f4e81293bb29e8e071817ed094883f856ebf94
zhuhaow <zhuhaow@gmail.com>
2024-10-08 20:58:52 +0800

    Fix formatting

--- core/dandelion-cli/src/bin/dandelion.rs
+++ core/dandelion-cli/src/bin/dandelion.rs
@@ -34,7 +34,9 @@ async fn main() -> Result<()> {
         }
     }
 
-    rustls::crypto::aws_lc_rs::default_provider().install_default().or_else(|_| Err(anyhow::anyhow!("Failed to install aws lc provider")))?;
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .or_else(|_| Err(anyhow::anyhow!("Failed to install aws lc provider")))?;
 
     let opt: Opt = Opt::from_args();
 
