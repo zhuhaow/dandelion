11ec258c122e90662130f426a1f0659c8d32e5c8
zhuhaow <zhuhaow@gmail.com>
2025-06-21 18:48:04 +0800

    Update dependencies

--- core/src/core/quic/client.rs
+++ core/src/core/quic/client.rs
@@ -26,7 +26,7 @@ pub async fn create_quic_connection<R: Resolver>(
             let host_ref = &host;
 
             let crypto_config = {
-                let mut config = rustls::ClientConfig::with_platform_verifier();
+                let mut config = rustls::ClientConfig::with_platform_verifier()?;
                 config.alpn_protocols = alpn_protocols;
                 Arc::new(QuicClientConfig::try_from(config)?)
             };
