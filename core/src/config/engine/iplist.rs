f359c2ce03997e4a60060854375301168d94b44a
zhuhaow <zhuhaow@gmail.com>
2025-06-22 21:53:20 +0800

    Add test for iplist

--- core/src/config/engine/iplist.rs
+++ core/src/config/engine/iplist.rs
@@ -1,10 +1,8 @@
-use std::{net::IpAddr, rc::Rc};
-
+use crate::config::rune::create_wrapper;
 use crate::Result;
 use ipnetwork::IpNetwork;
 use rune::{runtime::Vec as RuneVec, Any, FromValue, Module};
-
-use crate::config::rune::create_wrapper;
+use std::{net::IpAddr, rc::Rc};
 
 create_wrapper!(IpNetworkSetWrapper, Rc<Vec<IpNetwork>>);
 
@@ -55,5 +53,44 @@ impl IpNetworkSetWrapper {
         module.function_meta(Self::contains_any)?;
 
         Ok(module)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::config::engine::testing;
+
+    use super::*;
+    use rstest::*;
+
+    #[rstest]
+    #[case("10.0.0.0/8", "10.0.0.1", true)]
+    #[case("10.0.0.0/8", "12.0.0.1", false)]
+    #[case("2001:db8::/32", "2001:db8::1", true)]
+    #[case("2001:db8::/32", "2001:db9::1", false)]
+    #[case("127.0.0.1", "127.0.0.1", true)]
+    #[case("127.0.0.1", "127.0.0.2", false)]
+    #[case("::1/128", "::1", true)]
+    #[case("::1/128", "::2", false)]
+    #[tokio::test]
+    async fn test_iplist(
+        #[case] ipnetwork_str: &str,
+        #[case] ip_str: &str,
+        #[case] expected: bool,
+    ) -> Result<()> {
+        let result: bool = testing::run(
+            vec![IpNetworkSetWrapper::module()?],
+            &format!(
+                r#"
+                let iplist = new_iplist(["{ipnetwork_str}"])?;
+                iplist.contains("{ip_str}")?
+                "#,
+            ),
+        )
+        .await?;
+
+        assert_eq!(result, expected);
+
+        Ok(())
     }
 }
